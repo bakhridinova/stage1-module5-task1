eebff1abf51b8bde38b1577c89280bb1f02a9e57
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-22 17:06:31 +0500

    updated InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -7,26 +8,28 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return strings -> strings.stream().allMatch(string -> Character.isUpperCase(string.charAt(0)));
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.addAll(x.stream().filter(elem -> elem % 2 == 0).toList());
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> values.stream().filter(value -> Character.isUpperCase(value.charAt(0)) && value.contains(".") && value.split(" ").length > 3).toList();
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return strings -> strings.stream().collect(Collectors.toMap(Object::toString, String::length));
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (integers1, integers2) -> Stream.concat(integers1.stream(), integers2.stream()).toList();
     }
 }
